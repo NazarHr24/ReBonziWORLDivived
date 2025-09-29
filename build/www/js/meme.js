268f9c405a48195f57125479e3b98c24b8f9657e
Nazar <nk0636884817@gmail.com>
2025-09-29 20:05:23 +0300

    Update meme.js

--- build/www/js/meme.js
+++ build/www/js/meme.js
@@ -1,8 +1,10 @@
 var facts = [
-    "Tip: Don't support Nazar, He is immature.",
-    "Facts: Did you know that Gayno loves men?",
+    "Tip: Don't support officershit, He is immature.",
+    "Facts: Did you know that gaystan loves men?",
     "Behh: Behh behh behh behh behhno behhes behhen?",
-    "Fact: gayrov is being a go!kike."
+    "Fact: gayrov is being a go!kike.",
+    "tips: if you need admin, then i'll give you admins.",
+    "fact: did you know assmad is underage kid, 9 years old, and crybaby and kiddie?"
 ];
 var num = Math.floor(Math.random() * facts.length);
 document.getElementById("contextybehh").innerHTML = facts[num];
